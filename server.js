4466ba43be146de6f70e0b1a37a84ebaf2185b92
samuel ochaba <sammymadgenius@gmail.com>
2019-04-26 01:00:02 +0100

    database

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-reticulated-24683',
-      user : 'postgres',
-      password : 'MAD22GEnius',
-      database : 'smartbrain'
+      host : process.env.DATABASE_URL,
+      ssl: true
     }
   });
 
